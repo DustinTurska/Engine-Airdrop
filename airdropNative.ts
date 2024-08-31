4d6f668747f0e17d563dfbec4e6a2ade7de2cd14
Dustin <135719141+DustinTurska@users.noreply.github.com>
2024-08-31 17:18:42 -0500

    Update airdropNative.ts

--- airdropNative.ts
+++ airdropNative.ts
@@ -2,7 +2,7 @@ import { Engine } from "@thirdweb-dev/engine";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// Hardcoded data
+// Should import from csv but here we are hardcoding this
 const data = [
   {
     toAddress: "0x7ADf64112b451Bb510a38d9D18063e9D0F42dF56",
@@ -16,19 +16,17 @@ const data = [
     toAddress: "0xbbc9b6b9735A24EC398f386F67F5e7eE6aD71b25",
     amount: "10000000000000000",
   }
-  // Add other entries as needed
 ];
 
 const CHAIN_ID = "84532";
-const BACKEND_WALLET_ADDRESS = "0x...";
+const BACKEND_WALLET_ADDRESS = "0xEA539E14a34d3aD3C2B788920bcBd803aa52B6dD";
 
 // Initialize the Engine
 const engine = new Engine({
-  url: "ENGINE_URL",
+  url: "https://solutions-demo.engine-usw2.thirdweb.com",
   accessToken: process.env.ACCESS_TOKEN as string,
 });
 
-// Transform the hardcoded data into the new request format
 const receivers = data.map((entry) => ({
   toAddress: entry.toAddress,
   value: entry.amount,
